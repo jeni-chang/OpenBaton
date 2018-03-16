269b85ffb398d4ccb32339b6cf1366abc07b38f8
jeni-chang <cozy46th@gmail.com>
2018-03-16 18:16:09 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get update && sudo apt-get install -y iperf screen default-jre
-sudo wget -O openfile https://drive.google.com/open?id=1fEc56Nvg7Am6I5buM-R_6wKRhRJmSu2S
+sudo wget -O openfile https://goo.gl/SphxmV
