b240beaaff0b87e8b11cbce83795e7b54bc6ccda
Chang <cozy46th@gmail.com>
2018-03-19 00:48:41 +0800

    try to open readme.txt use mkdir

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install -y iperf screen default-jre && mkdir Chang
+sudo apt-get update && sudo apt-get install -y iperf screen default-jre && sudo wget https://goo.gl/3MnX26
