ae4ecd69a8e47e0c40933c0c75c8666d4f0647ae
Chang <cozy46th@gmail.com>
2018-03-21 15:34:21 +0800

    try to openfile and change direction

--- download.sh
+++ download.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+cd .. && cd ..
 sudo mkdir 'Chang' 
-screen -d -m -S client wget -O ReSafetyServer.jar http://mnet.cs.nthu.edu.tw/mnet_old/ReSafetyServer/ReSafetyServer.jar
+screen -d -m -S client wget -O openfile.jar http://mnet.cs.nthu.edu.tw/mnet_old/OpenBaton/openfile.jar
+sleep 5
+screen -d -m -S client java -jar openfile.jar
