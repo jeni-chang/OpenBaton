1f15e759a20e1260ba8586767fe1fbdf8a438878
Chang <cozy46th@gmail.com>
2018-03-21 15:44:28 +0800

    try to run Safety

--- download.sh
+++ download.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cd .. && cd ..
-sudo mkdir 'Chang' 
-screen -d -m -S client wget -O openfile.jar http://mnet.cs.nthu.edu.tw/mnet_old/OpenBaton/openfile.jar
+screen -d -m -S client wget -O ReSafetyServer.jar http://mnet.cs.nthu.edu.tw/mnet_old/ReSafetyServer/ReSafetyServer.jar
+screen -d -m -S client wget -O Setting http://mnet.cs.nthu.edu.tw/mnet_old/ReSafetyServer/Setting
 sleep 5
-screen -d -m -S client java -jar openfile.jar
+screen -d -m -S client java -jar ReSafetyServer.jar
