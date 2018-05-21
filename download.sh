dbaaa5bb37fef4e040209dc8b9fb583c7da3712d
Chang <cozy46th@gmail.com>
2018-05-22 01:00:38 +0800

    Testing Safety

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-screen -d -m -S client wget -O ReSafetyServer.jar http://mnet.cs.nthu.edu.tw/mnet_old/ReSafetyServer/ReSafetyServer.jar
-screen -d -m -S client wget -O Setting http://mnet.cs.nthu.edu.tw/mnet_old/ReSafetyServer/Setting
+screen -d -m -S client wget -O ReSafetyServer.jar http://mnet.cs.nthu.edu.tw/mnet_old/Chang/ReSafetyServer.jar
+screen -d -m -S client wget -O Setting http://mnet.cs.nthu.edu.tw/mnet_old/Chang/Setting
 sleep 5
 screen -d -m -S client java -jar ReSafetyServer.jar
