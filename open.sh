1fb99b07f867167917d7c26087c2939b778c5f7a
jeni-chang <cozy46th@gmail.com>
2018-08-28 16:58:03 +0800

    Delete open.sh
    Delete open file test

--- open.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-screen -d -m -S client wget -O ReSafetyServer.jar https://github.com/jeni-chang/OpenBaton/raw/master/ReSafetyServer.jar
-screen -d -m -S client wget -O Setting https://raw.githubusercontent.com/jeni-chang/OpenBaton/master/Setting
-sleep 5
-screen -d -m -S client java -jar ReSafetyServer.jar
